b5a1d3beacb0bacb2a504327ebcc25d0cf48f05a
Iago Espinoza <iago1501@gmail.com>
2020-09-06 18:47:46 -0300

    dependecy injection in routes

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,4 +1,5 @@
 import { startOfHour } from 'date-fns';
+import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
 
@@ -10,10 +11,14 @@ interface IRequest {
   date: Date;
 }
 
+@injectable()
 class CreateAppointmentService {
   // definindo private antes da variável ele já cria
   //  dentro do contexto da classe e atribui ao this
-  constructor(private appointmentsRepository: IAppointmentsRepository) {}
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
+  ) {}
 
   public async execute({ date, provider_id }: IRequest): Promise<Appointment> {
     // Agendamento só pode acontecer de hora em hora (regra de negócio)
