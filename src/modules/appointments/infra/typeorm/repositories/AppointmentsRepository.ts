268492a6855dbae5cc61915ca756dcba2456f9dc
Iago Espinoza <iago1501@gmail.com>
2020-09-19 19:05:21 -0300

    provider availability add

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -1,8 +1,10 @@
-import { getRepository, Repository } from 'typeorm';
+import { getRepository, Repository, Raw } from 'typeorm';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
-import ICreateAppointmentDTO from '@modules/appointments/dtos/iCreateAppointmentDTO';
+import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
 
+import IFindAllInMonthFromProviderDTO from '@modules/appointments/dtos/IFindAllInMonthFromProviderDTO';
+import IFindAllInDayFromProviderDTO from '@modules/appointments/dtos/IFindAllInDayFromProviderDTO';
 import Appointment from '../entities/Appointment';
 
 class AppointmentsRepository implements IAppointmentsRepository {
@@ -17,6 +19,54 @@ class AppointmentsRepository implements IAppointmentsRepository {
       where: { date },
     });
     return findAppointment;
+  }
+
+  public async findAllInMonthFromProvider({
+    provider_id,
+    month,
+    year,
+  }: IFindAllInMonthFromProviderDTO): Promise<Appointment[]> {
+    const parsedMonth = String(month).padStart(2, '0'); // 1 - 01, muito legal
+
+    const appointments = await this.ormRepository.find({
+      where: {
+        provider_id,
+        // Rawquery é uma query que não vai tentar ser interpretada pelo typeorm,
+        // vai ser passada direto pro BD
+
+        // datefieldname é pq o nome nunca vai ser o mesmo do typeORM
+        date: Raw(
+          dateFieldName =>
+            `to_char(${dateFieldName}, 'MM-YYYY') = '${parsedMonth}-${year}'`,
+        ),
+      },
+    });
+    return appointments;
+  }
+
+  public async findAllInDayFromProvider({
+    provider_id,
+    month,
+    year,
+    day,
+  }: IFindAllInDayFromProviderDTO): Promise<Appointment[]> {
+    const parsedMonth = String(month).padStart(2, '0'); // 1 - 01, muito legal
+    const parsedDay = String(day).padStart(2, '0'); // 1 - 01, muito legal
+
+    const appointments = await this.ormRepository.find({
+      where: {
+        provider_id,
+        // Rawquery é uma query que não vai tentar ser interpretada pelo typeorm,
+        // vai ser passada direto pro BD
+
+        // datefieldname é pq o nome nunca vai ser o mesmo do typeORM
+        date: Raw(
+          dateFieldName =>
+            `to_char(${dateFieldName}, 'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${year}'`,
+        ),
+      },
+    });
+    return appointments;
   }
 
   public async create({
